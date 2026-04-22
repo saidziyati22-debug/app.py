243fc836d9029e319ad6c07356bc923511b70f2a
saidziyati22-debug <saidziyati22@gmail.com>
2026-04-22 17:20:39 +0100

    Update greeting message in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 genai.configure(api_key="YOUR_API_KEY_HERE")
 
 st.title("My AI Assistant 🤖")
-st.write("مرحباً! أنا ذكاء اصطناعي خاص SAID ZIYATI.")
+st.write("مرحباً! أنا ذكاء اصطناعي خاص بسعد الزياتي..")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
